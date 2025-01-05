1fb74c9ec91435ac0c288a37a4da8afaf981d8a7
katoh-rikuto <maruhaya@hotmail.co.jp>
2025-01-05 16:59:56 +0900

    todo_service.jsを追加

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export const ApiUrls = {
+    getTodo:
+        "https://0bshe8ese1.execute-api.ap-northeast-1.amazonaws.com/default/GetTodo",
+    manageTodo:
+        "https://ojgu7oddz1.execute-api.ap-northeast-1.amazonaws.com/default/ManageTodo",
+};
